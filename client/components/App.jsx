909503a31ab3d7f82760b93b9fcb73a23b8ea14f
Andrew Vance <andrewaddress2@gmail.com>
2021-01-09 21:47:34 -0800

    emitting message on submit and receiving messages from sever and storing in state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MessageList from './MessageList';
 import SendMessage from './SendMessage';
+import socket from '../service/socket.js';
 
 const App = () => {
 
-  const socket = io();
+  const [messages, updateMessages] = useState(null);
+
+  useEffect(() => {
+    socket.once('messages', newMessage => {
+      console.log(newMessage);
+      let currentMessages;
+      if (!messages) {
+        currentMessages = [];
+      } else {
+        currentMessages = [...messages];
+      }
+      currentMessages.push(newMessage);
+      updateMessages(currentMessages);
+    });
+  }, [messages]);
 
   const submitMessage = (message) => {
     socket.emit('message', message);
@@ -12,7 +27,10 @@ const App = () => {
 
   return (
     <div>
-      <MessageList />
+      <div id='chat'>
+        <MessageList messages={messages}/>
+      </div>
+
       <SendMessage submitMessage={submitMessage}/>
     </div>
   );
