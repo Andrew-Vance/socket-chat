17efbffa0d5a4e388fc6e69c491fbbf5354e77d3
Andrew Vance <andrewaddress2@gmail.com>
2021-01-10 14:53:49 -0800

    rendering page depending on path

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Login from './components/Login';
+import Register from './components/Register';
 
-ReactDOM.render(< App />, document.getElementById('app'));
+if (window.location.pathname === '/') {
+  ReactDOM.render(< App />, document.getElementById('app'));
+} else if (window.location.pathname === '/login/') {
+  ReactDOM.render(< Login />, document.getElementById('app'));
+} else if (window.location.pathname === '/register/') {
+  ReactDOM.render(< Register />, document.getElementById('app'));
+}
