f46fddba37c17a7484e04b51a08ee4d3c7b0e8df
Andrew Vance <andrewaddress2@gmail.com>
2021-01-11 20:22:42 -0800

    setup mongoDB sessions and added route for registration

--- server/index.js
+++ server/index.js
@@ -1,40 +1,88 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-//const io = require('socket.io')(http);
+const User = require('../database');
 const socket = require('socket.io');
-const port = 3000;
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
+const mongo_URI = require('../database/config.js').MONGO_URI;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const port = process.env.port || 3000;
 
-app.use(express.static('client/dist'));
+const store = new mongoDBStore({
+  uri: mongo_URI,
+  collection: 'sessions'
+});
+
+store.on('error', err => {
+  console.log('error on store');
+});
+
+app.use(require('express-session')({
+  secret: 'secret',
+  cookie: {
+    maxAge: 1000 * 10 * 1
+  },
+  store: store,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/', express.static('client/dist'));
+app.use('/login', express.static('client/dist'));
+app.use('/register', express.static('client/dist'));
+
+
+
+let connectedUsers = {};
+
+app.get('/user/:id', (req, res) => {
+  User.get(req.params.id)
+    .then(user => {
+      console.log(user);
+      res.end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.end();
+    });
+});
+
+app.post('/register', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  User.create({ username, password })
+    .then(() => {
+      res.end();
+    })
+    .catch(err => {
+      console.log(err);
+      res.end();
+    });
+});
+
+app.get('/lobby', (req, res) => {
+  res.send(lobby);
+});
+
 
 const io = socket(app.listen(port, () => {
   console.log(`listening on ${port}`);
 }));
 
-let lobby = [];
-
-
-app.get('/lobby', (req, res) => {
-  res.send(lobby);
-})
-
-
-
-
 io.on('connection', (socket) => {
   console.log('connected');
   socket.join('lobby');
-  console.log(socket.rooms);
+  //console.log(socket.rooms);
 
 
   socket.on('message', (message) => {
-    console.log(message);
-    socket.emit('messages', message);
-    socket.to('lobby').emit('messages', message);
+    //console.log(message);
+    io.in('lobby').emit('messages', message);
   });
 });
-
-
-// http.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
